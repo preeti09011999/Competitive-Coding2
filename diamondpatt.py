a37ab9f1d2f912d364a5aed1220f248080574d9e
Preeti <preeti09011999@gmail.com>
2020-02-04 08:57:09 +0530

    Pattern problem statement(2)

---
+++ diamondpatt.py
@@ -0,0 +1,19 @@
+n = int(input())
+pos = 1
+flag = False
+for i in range(-n//2 + 1,n//2 + 1):
+    #printing spaces
+    for _ in range(abs(i)):
+        print(' ',end='')
+    #printing numbers
+    for j in range(pos,n-2*(abs(i))+1):
+        print(j,end='')
+    #printing in reverse order
+    for k in range(n-2*(abs(i))-1,pos-1,-1):
+        print(k,end='')
+    if i < 0:
+        pos += 1
+    else:
+        pos -= 1
+    print()
+
